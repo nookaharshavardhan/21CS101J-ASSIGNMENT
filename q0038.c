3fd98070be4aef6bc1552c1179ca455999d0af0a
harsha <nookaharshavardhan10@gmail.com>
2024-11-15 21:35:08 +0530

    initial commit

--- q0038.c
+++ q0038.c
@@ -1,26 +1,30 @@
 // C program to check if a number is palindrome
 #include<stdio.h>
 #include<string.h>
-int main()
+int main ()
 {
-    char temp;
-    char string[100];
-   printf("give the string\n");
-   gets(string);
-   int size=strlen(string);
-   char string1[100];
-   for(int i=0,j=size-1;i<size;i++,j--)
-   {
-       
-        string1[i]=string[j];
+     int i,j;
+     char str[100];
+     gets(str);
+     int len=strlen(str);
+     printf("%d\n",len);
+     char str1[len];
+     for(i=0,j=len-1;j>=0; i++, j--)
+     {
+          str1[i]=str[j];
+     }
+    
      
-   }
-   string1[size]='\0';
-   if(strcmp(string,string1)==0)
-   {
-    printf("yes it is a palandrome \n");
-   }
-   else
-      printf("no it is not a palandrome\n");
-
+     if(strcmp(str1,str)==0)
+     {
+          printf("it is a palindrome\n");
+     }
+     else{
+          printf("it is not a palindrome\n");
+     }
+    
+     for(int i=0;i<len;i++)
+     {
+          printf("%c",str1[i]);
+     }printf("\n");
 }
